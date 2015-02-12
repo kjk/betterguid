03c9abbdc1a9de2a7f2eac5eeea7bbed904369fa
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-02-12 13:45:29 -0800

    more doc

--- doc.go
+++ doc.go
@@ -3,20 +3,15 @@
 Package betterguid generates 20-character guid (globally unique id) strings
 with good properties:
 
-* they're 20 character strings, safe for inclusion in urls (don't require escaping)
+They're 20 character strings, safe for inclusion in urls (don't require escaping)
 
-* they're based on timestamp so that they sort **after** any existing ids
+They're based on timestamp so that they sort **after** any existing ids
 
-* they contain 72-bits of random data after the timestamp so that IDs won't
-  collide with other clients' IDs
+They contain 72-bits of random data after the timestamp so that IDs won't collide with other clients' IDs
 
-* they sort **lexicographically** (so the timestamp is converted to characters
-  that will sort properly)
+They sort **lexicographically** (so the timestamp is converted to characters that will sort properly)
 
-* they're monotonically increasing.  Even if you generate more than one in the
-  same timestamp, thelatter ones will sort after the former ones.  We do this
-  by using the previous random bits but "incrementing" them by 1 (only in the
-  case of a timestamp collision).
+They're monotonically increasing.  Even if you generate more than one in the same timestamp, thelatter ones will sort after the former ones.  We do this by using the previous random bits but "incrementing" them by 1 (only in the case of a timestamp collision).
 
 Read https://www.firebase.com/blog/2015-02-11-firebase-unique-identifiers.html
 for more info.
