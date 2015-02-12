dfb1ca1b048f3684159ab9d72ed940a4d2280ee8
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2015-02-12 13:44:09 -0800

    try fix godoc

--- doc.go
+++ doc.go
@@ -2,12 +2,17 @@
 
 Package betterguid generates 20-character guid (globally unique id) strings
 with good properties:
+
 * they're 20 character strings, safe for inclusion in urls (don't require escaping)
+
 * they're based on timestamp so that they sort **after** any existing ids
+
 * they contain 72-bits of random data after the timestamp so that IDs won't
   collide with other clients' IDs
+
 * they sort **lexicographically** (so the timestamp is converted to characters
   that will sort properly)
+
 * they're monotonically increasing.  Even if you generate more than one in the
   same timestamp, thelatter ones will sort after the former ones.  We do this
   by using the previous random bits but "incrementing" them by 1 (only in the
